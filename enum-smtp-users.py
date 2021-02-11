80915c6c1319f13522816c7535e5338936180fb9
Blake Balick-Schreiber <blake.balick.schreiber@gmail.com>
2021-02-10 23:17:07 -0500

    added script to enum users over smtp

---
+++ enum-smtp-users.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import socket
+import sys
+
+if len(sys.argv) != 3:
+    print "Usage: vrfy.py <file-with-usernames> <file-with-ips>"
+    sys.exit(0)
+
+def enum_users(userFile, ipFile):
+    with open(userFile, 'r') as uf:
+        with open(ipFile, 'r') as ipf
+        usernames = uf.readlines()
+        ips = ipf.readlines()
+        for ip in ips:
+            #create socket
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #connect to server
+            connect = s.connect((ip,25))
+            #receive banner
+            banner = s.recv(1024)
+            #print banner
+            print(banner)
+            for user in usernames:
+                #verify a user
+                s.send('VRFY' + user + '\r\n')
+                result = s.recv(1024)
+                print(result)
+            s.close()
+            
